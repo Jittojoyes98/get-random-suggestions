59427db32d4843c61fe7ea3e0ab11b940662fd41
Jitto Joyes <jittojoyes77@gmail.com>
2024-01-19 17:06:13 +0530

    add and release
    -- restaurants and malls
    -- version 1.3.0 release.

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-const fs=require("fs")
-const tourData=require("./tour.json")
+const fs = require("fs");
+const tourData = require("./tour.json");
+const restaurantData = require("./restaurants.json");
+const mallData = require("./malls.json");
 
-function getRandomSuggestions(type){
-    let length,index,place;
-    if(type==="tour"){
-        length = tourData.length;
-        index = Math.floor(Math.random() * length);
-        place = tourData[index];
-        return place;
-    }
-    else{
-        return "Please set a valid parameter"
-    }
+function getRandomSuggestions(type) {
+  let length, index, data;
+  length = 500;
+  index = Math.floor(Math.random() * length);
+  if (type === "tour") {
+    data = tourData[index];
+    return data;
+  } else if (type === "restaurants") {
+    data = restaurantData[index];
+    return data;
+  } else if (type === "malls") {
+    data = mallData[index];
+    return data;
+  } else {
+    return "Please set a valid parameter";
+  }
 }
 
-module.exports = getRandomSuggestions
+module.exports = getRandomSuggestions;
--- mock/script.js
+++ mock/script.js
@@ -3,3 +3,9 @@ const suggest = require("get-random-suggestions");
 console.log(suggest("tour"));
 // output
 // { area: 'Chhattisgarh', place: 'Patna' }
+console.log(suggest("restaurants"));
+// output
+// { place: 'Bangalore', restaurant_name: 'Zesty Zest' }
+console.log(suggest("malls"));
+// output
+// { place: 'Chennai', mall_name: 'Elite Vogue Hub' }
