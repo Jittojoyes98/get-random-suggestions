7cb08b5c6666ec878c60e00c7c2f6895a43326bb
Jitto Joyes <jittojoyes77@gmail.com>
2024-01-20 08:44:35 +0530

    add
    -- baby names
    -- version 1.4.0 release.

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-const fs = require("fs");
-const tourData = require("./tour.json");
-const restaurantData = require("./restaurants.json");
-const mallData = require("./malls.json");
+const fs = require('fs')
+const tourData = require('./tour.json')
+const restaurantData = require('./restaurants.json')
+const mallData = require('./malls.json')
+const babyGirlsData = require('./babygirls.json')
+const babyBoysData = require('./babyboys.json')
 
 function getRandomSuggestions(type) {
-  let length, index, data;
-  length = 500;
-  index = Math.floor(Math.random() * length);
-  if (type === "tour") {
-    data = tourData[index];
-    return data;
-  } else if (type === "restaurants") {
-    data = restaurantData[index];
-    return data;
-  } else if (type === "malls") {
-    data = mallData[index];
-    return data;
+  let length, index, data
+  length = 500
+  index = Math.floor(Math.random() * length)
+  if (type === 'tour') {
+    data = tourData[index]
+    return data
+  } else if (type === 'restaurants') {
+    data = restaurantData[index]
+    return data
+  } else if (type === 'malls') {
+    data = mallData[index]
+    return data
+  } else if (type === 'babygirls') {
+    data = babyGirlsData[index]
+    return data
+  } else if (type === 'babyboys') {
+    data = babyBoysData[index]
+    return data
   } else {
-    return "Please set a valid parameter";
+    return 'Please set a valid parameter'
   }
 }
 
-module.exports = getRandomSuggestions;
+module.exports = getRandomSuggestions
--- mock/script.js
+++ mock/script.js
@@ -1,11 +1,17 @@
 // script.js
-const suggest = require("get-random-suggestions");
-console.log(suggest("tour"));
+const suggest = require('get-random-suggestions')
+console.log(suggest('tour'))
 // output
 // { area: 'Chhattisgarh', place: 'Patna' }
-console.log(suggest("restaurants"));
+console.log(suggest('restaurants'))
 // output
 // { place: 'Bangalore', restaurant_name: 'Zesty Zest' }
-console.log(suggest("malls"));
+console.log(suggest('malls'))
+// output
+// { name: 'Aaliyah', meaning: 'Exalted, Noble', religion: 'Muslim' }
+console.log(suggest('babygirls'))
 // output
 // { place: 'Chennai', mall_name: 'Elite Vogue Hub' }
+console.log(suggest('babyboys'))
+// output
+// { name: 'Zain', meaning: 'Grace', religion: 'Muslim' }
